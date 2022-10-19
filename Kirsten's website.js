21dcc88a7f785d199d509d5f418baf727fb7cbf1
JayraldC <116184467+JayraldC@users.noreply.github.com>
2022-10-19 22:43:47 +0800

    Add files via upload

---
+++ Kirsten's website.js
@@ -0,0 +1,27 @@
+	var activeImageId = 1;
+	var nextImageId = 1;
+	
+	setInterval(function(){
+	nextImageId = nextImageId + 1;
+	 
+	if(nextImageId < 6) {
+		
+	document.getElementById("img"+activeImageId).classList.replace("visible","hidden");
+
+
+	document.getElementById("img"+nextImageId).classList.replace("hidden","visible");
+		
+	activeImageId = nextImageId;
+	} else {
+	  
+	document.getElementById("img"+activeImageId).classList.replace("visible","hidden");
+
+
+	document.getElementById("img"+nextImageId).classList.replace("hidden","visible");
+	  
+	activeImageId = 6;
+	nextImageId = 0;
+		
+	}
+	
+},1000);
